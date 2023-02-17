4db00d1871f48d8972483219ea493747523aca22
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-17 12:44:26 +0100

    chore: create surgery booking entity

--- src/main/kotlin/entities/SurgeryBooking.kt
+++ src/main/kotlin/entities/SurgeryBooking.kt
@@ -8,40 +8,70 @@
 
 package entities
 
+import java.time.LocalDateTime
+
 /**
  * A surgery booking for an elective interventions.
  * @param surgeryID the id of the surgery.
  * @param surgeryType the type of the surgery.
- * @param patientTaxCode the tax code of the patient.
+ * @param healthcareUserID the tax code of the patient.
  * @param healthProfessionalID the id of the health professional.
+ * @param surgeryDateTime the date time of the surgery.
  */
 class SurgeryBooking(
     val surgeryID: SurgeryID,
     val surgeryType: SurgeryType,
-    val patientTaxCode: PatientTaxCode,
-    val healthProfessionalID: HealthProfessionalID
+    val healthcareUserID: HealthcareUserID,
+    val healthProfessionalID: HealthProfessionalID,
+    val surgeryDateTime: SurgeryDateTime
 )
 
 /**
  * The id of the surgery.
  * @param id the id.
  */
-data class SurgeryID(val id: String)
+data class SurgeryID(val id: String) {
+    init {
+        require(id.isNotEmpty())
+    }
+}
 
 /**
  * The type of the surgery.
  * @param type the type of the surgery.
  */
-data class SurgeryType(val type: String)
+data class SurgeryType(val type: String) {
+    init {
+        require(type.isNotEmpty())
+    }
+}
 
 /**
- * The tax code of the patient.
- * @param code the patient tax code.
+ * The id of the healthcare user.
+ * @param id the healthcare user id.
  */
-data class PatientTaxCode(val code: String)
+data class HealthcareUserID(val id: String) {
+    init {
+        require(id.isNotEmpty())
+    }
+}
 
 /**
  * The id of the health professional assigned for the intervention.
  * @param id the id of the health professional.
  */
-data class HealthProfessionalID(val id: String)
+data class HealthProfessionalID(val id: String) {
+    init {
+        require(id.isNotEmpty())
+    }
+}
+
+/**
+ * The date time of the surgery.
+ * @param date the id of the health professional.
+ */
+data class SurgeryDateTime(val date: LocalDateTime) {
+    init {
+        require(date.isAfter(LocalDateTime.now()))
+    }
+}

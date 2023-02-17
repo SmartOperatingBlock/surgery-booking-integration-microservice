b6be413ff720300848a6f7b67ca8d4c85857be9e
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-02-17 12:42:29 +0100

    chore: add simulated surgery booking message

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,3 +1,7 @@
+import application.SurgeryBookingController
+import infrastructure.DigitalTwinSurgeryBookingManager
+import infrastructure.SurgicalBookingDataReceiver
+
 /*
  * Copyright (c) 2023. Smart Operating Block
  *
@@ -10,5 +14,17 @@
  * Template for kotlin projects.
  */
 fun main() {
-    println("Hello World from Kotlin Template")
+    val surgeryBookingManager = DigitalTwinSurgeryBookingManager()
+    val surgeryBookingController = SurgeryBookingController(surgeryBookingManager)
+    val surgeryBookingDataReceiver = SurgicalBookingDataReceiver(surgeryBookingController)
+    val dataSimulation = """
+        {
+        	"surgeryID" : "surgery1", 
+        	"surgeryType" : "aneurysmClipping", 
+        	"healthcareUserID" : "hu1",
+        	"healthProfessionalID" : "hp1",
+        	"surgeryDateTime" : "2023-02-17T09:18:33" 
+        }
+    """.trimIndent()
+    surgeryBookingDataReceiver.receiveSurgeryBookingInformation(dataSimulation)
 }

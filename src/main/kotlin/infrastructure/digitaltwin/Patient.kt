c7f80ada97b72132cfa0eca23ab98db72c3fe02a
giacomo accursi <giacomo.accursi@studio.unibo.it>
2023-03-23 14:25:24 +0100

    chore: add patient representation for azure dt

---
+++ src/main/kotlin/infrastructure/digitaltwin/Patient.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023. Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+package infrastructure.digitaltwin
+
+/**
+ * Digital twin representation of a patient.
+ * @param id the id.
+ */
+data class Patient(val id: String)
